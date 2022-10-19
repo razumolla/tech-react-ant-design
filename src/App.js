df9090d2866373ebd5ab3b8f522954f904268976
razumolla <rajuahmed9344@gmail.com>
2022-10-19 10:21:38 +0600

    ant design heder added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'antd/dist/antd.css';
+import { Layout } from 'antd';
+import AppHeader from './components/Common/AppHeader';
+import Home from './components/Views/Home';
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="mainLayout">
+      <Header>
+        <AppHeader></AppHeader>
+      </Header>
+      <Content>
+        <Home></Home>
+      </Content>
+    </Layout>
   );
 }
 
