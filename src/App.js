41fc632d348baeab1fa9b720c58103e0a934c190
razumolla <rajuahmed9344@gmail.com>
2022-10-19 17:30:30 +0600

    Footer added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'antd/dist/antd.css';
 import { Layout } from 'antd';
 import AppHeader from './components/Common/AppHeader';
 import Home from './components/Views/Home';
+import AppFooter from './components/Common/AppFooter';
 const { Header, Content, Footer } = Layout;
 
 function App() {
@@ -15,6 +16,9 @@ function App() {
       <Content>
         <Home></Home>
       </Content>
+      <Footer>
+        <AppFooter></AppFooter>
+      </Footer>
     </Layout>
   );
 }
---
+++ src/components/Common/AppFooter.js
@@ -0,0 +1,30 @@
+import { BackTop } from 'antd';
+import React from 'react';
+
+const AppFooter = () => {
+    return (
+        <div className="container-fluid">
+            <div className="footer">
+                <div className="logo">
+                    <i className="fas fa-bolt"></i>
+                    <a href="">Tech</a>
+                </div>
+                <ul className="socials">
+                    <li><a href="https://www.facebook.com/"><i className="fab fa-facebook-f"></i></a></li>
+                    <li><a href="https://www.twitter.com/"><i className="fab fa-twitter"></i></a></li>
+                    <li><a href="https://www.linkedin.com/"><i className="fab fa-linkedin-in"></i></a></li>
+                    <li><a href="https://www.pinterest.com/"><i className="fab fa-pinterest-p"></i></a></li>
+                    <li><a href="https://www.instagram.com/"><i className="fab fa-instagram"></i></a></li>
+                </ul>
+                <div className="copyright">Copyright &copy; 2022 , All Right Reserved by Razu Molla</div>
+                <BackTop>
+                <div className="goTop"> <i className="fas fa-arrow-circle-up"></i></div>
+                </BackTop>
+
+
+        </div>
+        </div >
+    );
+};
+
+export default AppFooter;
